a5ff50bb265a11db53d8a756f7ed14e4b870594f
josemanuelsoltero <josemanuelsoltero@joses-MBP.lan>
2023-03-03 01:20:05 -0600

    replaced my code with the the starter code

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,15 @@
+require('dotenv').config();
+
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_PASSWORD,
-    process.env.DB_USER,
-    {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3001
-    }
-);
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
-module.export= sequelize;
+module.exports = sequelize;
--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,6 @@
-DROP DATABASE IF EXIST ecommerce_db;
+-- DROP DATABASE
+DROP DATABASE IF EXISTS ecommerce_db;
+
+-- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
+
