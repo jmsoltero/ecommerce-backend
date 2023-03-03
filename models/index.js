4d112478f3d7c79904f0b86ed9083200a1857cfd
josemanuelsoltero <josemanuelsoltero@joses-MBP.lan>
2023-03-03 16:55:31 -0600

    added functionality to cat route

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreingKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreingKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreingKey: 'product_id',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreingKey: 'tag_id',
+});
 
 module.exports = {
   Product,
